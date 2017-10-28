6d8cdb8878982df39a49ae308ee62c17502f22d6
smakosh <ismai23l@hotmail.com>
2017-10-28 01:16:13 +0100

    dynamic content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './components/post/post.component';
-import { HaircutComponent } from './components/haircut/haircut.component';
-import { ElonComponent } from './components/elon/elon.component';
 import { ListComponent } from './components/list/list.component';
 
 import { PostService } from './services/post.service';
@@ -15,8 +13,6 @@ import { TimelineComponent } from './components/timeline/timeline.component';
   declarations: [
     AppComponent,
     PostComponent,
-    HaircutComponent,
-    ElonComponent,
     ListComponent,
     TimelineComponent
   ],
